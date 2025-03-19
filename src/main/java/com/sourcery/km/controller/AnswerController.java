fff67f73bf2c1aee02ce7475920d0125b1203117
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2025-03-19 21:32:57 +0200

    JSF2025S-2: Auth0 tenant setup

--- src/main/java/com/sourcery/km/controller/AnswerController.java
+++ src/main/java/com/sourcery/km/controller/AnswerController.java
@@ -1,15 +1,18 @@
 package com.sourcery.km.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*")
 public class AnswerController {
 
-  @GetMapping("/api")
+  @GetMapping("/public")
   public String getAnswer() {
-    return "Hello from Spring Boot!!";
+    return "Hello from Spring Boot!! ";
   }
 }
