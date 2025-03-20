459eb6d0daac8070840df7057b18e4ff33a9ab29
Renaldas Narbutas <renaldas.work@proton.me>
2025-03-20 15:03:40 +0200

    JSF2025S-8: pridėta liquibase

--- src/main/java/com/sourcery/km/SourceryApplication.java
+++ src/main/java/com/sourcery/km/SourceryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SourceryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SourceryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SourceryApplication.class, args);
+    }
 
 }
