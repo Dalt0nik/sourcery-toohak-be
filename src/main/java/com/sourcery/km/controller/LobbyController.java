b38f5ea644790542380a95de9fa72795df4862d2
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2025-03-25 16:48:34 +0200

    JSF2025S-12: Refactor code structure and comments

--- src/main/java/com/sourcery/km/controller/LobbyController.java
+++ src/main/java/com/sourcery/km/controller/LobbyController.java
@@ -3,7 +3,6 @@ package com.sourcery.km.controller;
 import com.sourcery.km.dto.NewQuestionDTO;
 import com.sourcery.km.service.LobbyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +16,6 @@ public class LobbyController {
      * Starts the game in the specified lobby and notifies all subscribed clients via WebSocket.
      *
      * @param lobbyId The ID of the lobby where the game is to be started.
-     * @return A response confirming that the game has started.
      */
     @PostMapping("/{lobbyId}/start")
     public void startGame(@PathVariable String lobbyId) {
@@ -30,13 +28,10 @@ public class LobbyController {
      *
      * @param lobbyId The ID of the lobby where the question should be sent.
      * @param question The question data to send, including lobbyId and question text.
-     * @return A response confirming that the question has been sent.
      */
     @PostMapping("/{lobbyId}")
-    public ResponseEntity<String> sendQuestion(@PathVariable String lobbyId, @RequestBody NewQuestionDTO question) {
+    public void sendQuestion(@PathVariable String lobbyId, @RequestBody NewQuestionDTO question) {
 
         lobbyService.sendNewQuestion(lobbyId, question);
-
-        return ResponseEntity.ok("New question has been sent!");
     }
 }
