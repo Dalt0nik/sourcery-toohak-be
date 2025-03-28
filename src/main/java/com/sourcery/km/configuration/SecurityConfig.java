15f5ae2349bc4dd87a16e27e3a3c5e712680d74b
Tomas <tbdgacc@gmail.com>
2025-03-28 23:24:52 +0200

    JSF2025S-36-api-backend-fix: Changed SecurityConfig to allow localhost access to APIs

--- src/main/java/com/sourcery/km/configuration/SecurityConfig.java
+++ src/main/java/com/sourcery/km/configuration/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
