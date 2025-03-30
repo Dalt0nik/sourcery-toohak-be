c8af9a73c260826d6aaf99b27c7bd7d2221b2c78
Anton Isakov <anton.isakov23@gmail.com>
2025-03-30 22:56:02 +0300

    global exception handler added

--- src/main/java/com/sourcery/km/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sourcery/km/exception/GlobalExceptionHandler.java
@@ -1,2 +1,19 @@
-package com.sourcery.km.exception;public class GlobalExceptionHandler {
+package com.sourcery.km.exception;
+
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({QuizNotFoundException.class})
+    public ProblemDetail handleQuizNotFoundException (QuizNotFoundException exception){
+        ProblemDetail response = ProblemDetail.forStatus(404);
+        response.setTitle("Quiz not found");
+        response.setStatus(response.getStatus());
+        response.setDetail(exception.getMessage());
+        return response;
+    }
 }
