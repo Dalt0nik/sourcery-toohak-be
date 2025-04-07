cb958d1bbdb4b86e21fd43b8edd2c08dc16e9bac
Rokas058 <rokas.prabusas@gmail.com>
2025-04-07 20:23:21 +0300

    JSF2025S-68: create question with question options

--- src/main/java/com/sourcery/km/service/QuestionService.java
+++ src/main/java/com/sourcery/km/service/QuestionService.java
@@ -5,11 +5,13 @@ import com.sourcery.km.dto.question.CreateQuestionDTO;
 import com.sourcery.km.entity.Question;
 import com.sourcery.km.entity.Quiz;
 import com.sourcery.km.repository.QuestionRepository;
+import com.sourcery.km.service.helper.QuestionOptionHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -20,12 +22,16 @@ public class QuestionService {
 
     private final QuizService quizService;
 
+    private final QuestionOptionHelper questionOptionHelper;
+
     @Transactional
     public void insertQuestion(CreateQuestionDTO questionDTO, UUID quizId) {
         Quiz quiz = quizService.getQuiz(quizId);
         quizService.isQuizCreator(quiz);
 
         Question question = QuestionBuilder.toQuestionEntity(questionDTO);
+        quiz.setQuestions(List.of(question));
         questionRepository.insertQuestion(question);
+        questionOptionHelper.insertQuestionOptions(quiz);
     }
 }
